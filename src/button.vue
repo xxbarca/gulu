8b4ef9924dfa2910ca8d9399a3e418de4cdf2c5c
李杨 <wxly@yonyou.com>
2020-06-10 21:19:33 +0800

    验证 iconPosition 为 left 或者 right 中的一个

--- src/button.vue
+++ src/button.vue
@@ -12,7 +12,16 @@
 
 <script>
 	export default {
-		props: ['icon', 'iconPosition']
+		props: {
+			icon: {},
+			iconPosition: {
+				type: String,
+				default: 'left',
+				validator(value) {
+					return !(value !== 'left' && value !== 'right')
+				}
+			}
+		}
 	}
 
 </script>
