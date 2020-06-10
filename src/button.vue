574db24981e8be35ae1c5197ba2da1328b9a0464
李杨 <wxly@yonyou.com>
2020-06-10 21:33:08 +0800

    更新 Button 里面 Icon 的样式

--- src/button.vue
+++ src/button.vue
@@ -1,17 +1,16 @@
 <template>
 	<button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-		<svg v-if="icon" class="icon">
-			<use :xlink:href=`#i-${icon}`></use>
-		</svg>
+		<g-icon class="icon" v-if="icon" :name="icon"></g-icon>
 		<div class="content">
 			<slot></slot>
-
 		</div>
 	</button>
 </template>
 
 <script>
+	import Icon from "./icon"
 	export default {
+		components: {Icon},
 		props: {
 			icon: {},
 			iconPosition: {
