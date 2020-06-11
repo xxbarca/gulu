af9a886b2fa5cd68cb65a4dfa7ab78298a047da7
liyang <xxbarca@163.com>
2020-06-11 22:36:48 +0800

    使用chai添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import chai from 'chai'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -13,3 +14,87 @@ new Vue({
 		loading1: false
 	}
 })
+
+const expect = chai.expect
+
+{
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: "settings"
+		}
+	})
+	vm.$mount()
+	let useElement = vm.$el.querySelector('use')
+	expect(useElement.getAttribute('xlink:href')).to.eq("#i-settings")
+	vm.$el.remove()
+	vm.$destroy()
+}
+
+{
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: "settings",
+			loading: true
+		}
+	})
+	vm.$mount()
+	let useElement = vm.$el.querySelector('use')
+	let href = useElement.getAttribute("xlink:href")
+	expect(href).to.eq('#i-loading')
+	vm.$el.remove()
+	vm.$destroy()
+}
+
+{
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: "settings"
+		}
+	})
+	vm.$mount(div)
+	let svg = vm.$el.querySelector('svg')
+	let {order} = window.getComputedStyle(svg)
+	expect(order).to.eq("1")
+	vm.$el.remove()
+	vm.$destroy()
+}
+
+{
+	const div = document.createElement('div')
+	document.body.appendChild(div)
+	const Constructor = Vue.extend(Button)
+	const vm = new Constructor({
+		propsData: {
+			icon: "settings",
+			iconPosition: "right"
+		}
+	})
+	vm.$mount(div)
+	let svg = vm.$el.querySelector('svg')
+	let {order} = window.getComputedStyle(svg)
+	expect(order).to.eq("2")
+	vm.$el.remove()
+	vm.$destroy()
+}
+
+{
+	const Constructor = Vue.extend(Button)
+	const gButton = new Constructor({
+		propsData: {
+			icon: "settings"
+		}
+	})
+	gButton.$mount()
+	gButton.$on('click', function () {
+		console.log(1)
+	})
+	let button = gButton.$el
+	button.click()
+	gButton.$el.remove()
+	gButton.$destroy()
+}
